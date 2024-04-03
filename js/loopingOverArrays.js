70b861663bbc3af57dab4c5d77934e65a34162d2
Carolista <cjones.renaissancewoman@gmail.com>
2024-04-03 14:26:55 -0400

    created starter code for loopingOverArrays

---
+++ js/loopingOverArrays.js
@@ -0,0 +1,35 @@
+const input = require('readline-sync');
+
+
+// Example 1 - Iterating over an array and accessing each element with bracket notation
+
+let typesOfApples = ["Fuji", "Red Delicious", "MacIntosh", "Gala"];
+
+// TODO: Loop over the types of apples and print each one in a sentence.
+
+
+// Example 2 - Using more than one array of the same length and incorporating input.question()
+
+let clientInfoNeeded = ["First Name: ", "Last Name: ", "Email Address: ", "Phone Number: "];
+
+// TODO: Create variables for each piece of data
+
+// TODO: Put all four variables in an array in the same order as their prompt counterpart
+
+// TODO: Create a line of 32 asterisks to use for final printout
+
+// TODO: Ask for contact information and save to each variable in the second array
+
+// TODO: Print a final recap to the user with each piece of data on a new line using a loop
+
+
+
+// Example 3 - Copying elements from one array to another
+
+let nationalParks = ["Yellowstone", "Gateway Arch", "Glacier", "Zion", "Shenandoah", "Everglades"];
+
+// TODO: Create a variable to hold the full names
+
+// TODO: Create a new string for each park with its full name and put it in the new array
+
+// TODO: Print the new array
