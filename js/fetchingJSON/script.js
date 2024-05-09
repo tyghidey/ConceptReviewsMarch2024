804ebfaad622b9297cacd1f0526f5eff6dfe4ad0
Carolista <cjones.renaissancewoman@gmail.com>
2024-05-09 13:46:57 -0400

    added fetch examples

---
+++ js/fetchingJSON/script.js
@@ -0,0 +1,26 @@
+/* FETCHING JSON */
+
+// Use this URL as an endpoint:
+// https://docs.google.com/document/d/1DjGYPISRGCsP4zHwJYBtciqbKAvae1CZ9EKsci4mTJk/export?format=txt
+
+window.addEventListener("load", () => {
+
+  // OLD SCHOOL - .then()
+  
+  const oldSchool = document.getElementById("old-school");
+
+  // TODO: Fetch plant data and print it
+  // Add HTML to page for each plant with simple sentence
+  // Ex: "There are 16 red tulip bulbs available." 
+  
+  
+  // MODERN - async & await
+
+  const modern = document.getElementById("modern");
+  
+  // TODO: Create an asynchronous function to fetch plant data and print it
+  // Use .forEach() to put a div on the page for each plant with image and text
+  
+  // TODO: Don't forget to call the async function
+
+});
