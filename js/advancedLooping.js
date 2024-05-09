8531c68550d514b35b9079804c6807de6d202213
Carolista <cjones.renaissancewoman@gmail.com>
2024-05-09 13:47:55 -0400

    added advanced looping examples

---
+++ js/advancedLooping.js
@@ -0,0 +1,18 @@
+/* ADVANCED LOOPING */
+
+let books = [
+  {id: 2944, title: "Divergent", numPages: 487},
+  {id: 2945, title: "Insurgent", numPages: 525},
+  {id: 2946, title: "Allegiant", numPages: 526}
+];
+
+// TODO: Use a regular for loop to print the titles of each book
+
+
+// TODO: Use a for-of loop with a nested for-in loop to print all three properties of each object
+
+
+// TODO: Use .map() to produce an array with a sentence for each book and store it in a variable
+
+
+// TODO: Use .forEach() to print each sentence from the new array
