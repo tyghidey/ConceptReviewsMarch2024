f2879ae670500b06d0d0aa7a726a65af174e614b
Carolista <cjones.renaissancewoman@gmail.com>
2024-05-09 13:49:24 -0400

    added destructuring examples

---
+++ js/destructuring.js
@@ -0,0 +1,36 @@
+/* DESTRUCTURING */
+
+// ARRAYS
+
+let languages = ["JavaScript", "Java", "Python", "C#", "Ruby"];
+
+// TODO: Use destructuring to create variables for each element in the array
+
+// TODO: Print "JavaScript" and "Ruby" using the new variables
+
+
+// OBJECTS
+
+let contactInfo = {
+  name: "P. Sherman",
+  address: {
+    street: "42 Wallaby Way",
+    city: "Sydney",
+    country: "Australia"
+  }
+};
+
+// TODO: Use destructuring to create variable for name
+
+// TODO: Use destructuring to create variables for street, city, and country
+
+// TODO: Print "P. Sherman" and "Sydney" using the new variables
+
+
+// IMPORTING AN EXPORT OBJECT
+
+// TODO: Use common import syntax with destructuring to import all constants and functions from modules
+
+// TODO: Print both constants, rounding PI to 3.1416
+
+// TODO: Print a random number between 5-10, rounded to 2 decimal places
